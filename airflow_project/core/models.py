9d705cbeeda666a587414e213be1bccd935e4ad2
AlexMackey1 <C22739165@mytudublin.ie>
2025-11-18 20:30:43 +0000

    updated models.py

--- airflow_project/core/models.py
+++ airflow_project/core/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Airport(models.Model):
+    """Airport model with spatial location"""
+    iata_code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=200)
+    location = models.PointField()  # PostGIS Point field
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.iata_code})"
+
+    class Meta:
+        ordering = ['name']
+
+
+class PassengerHeatmapData(models.Model):
+    """Fake passenger data for heatmap visualization"""
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    passenger_count = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.airport.iata_code} - {self.timestamp} - {self.passenger_count} pax"
+
+    class Meta:
+        ordering = ['-timestamp']
